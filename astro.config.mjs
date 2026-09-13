11f4ceba01cc4b373669fff7609ac8c7f478ccb8
yuruys <yuruys.game@gmail.com>
2026-09-13 15:51:55 +0900

    Add guide: article-name

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
 
 			components: {
 				Header: './src/components/StarlightHeader.astro',
+				MobileMenuToggle: './src/components/WikiSidebarToggle.astro',
 				ThemeProvider: './src/components/NugThemeProvider.astro',
 			},
 
